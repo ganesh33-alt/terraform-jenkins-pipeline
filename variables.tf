6e7b5026e5b79b1d363c91b6589de2fed04fa960
ganesh33-alt <anjalianke33@gmail.com>
2025-02-14 12:42:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  default     = ""
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  default     = ""
-}
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
